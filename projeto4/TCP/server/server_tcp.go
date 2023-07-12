5cd2d1c5c495b8a2a86eccdb0bded7d85fbb2dff
gugaccampos <gugaccampos13@gmail.com>
2023-07-12 18:54:50 -0300

    Exercício 4 1º modelo

--- projeto4/TCP/server/server_tcp.go
+++ projeto4/TCP/server/server_tcp.go
@@ -4,79 +4,34 @@ import (
 	"encoding/json"
 	"fmt"
 	"net"
+	"os"
 	//"strconv"
 )
-
-type NumbersRequest struct {
-	Numbers []int `json:"numbers"`
-}
 
 type NumbersResponse struct {
 	PrimeNumbers    []int `json:"prime_numbers"`
 	NonPrimeNumbers []int `json:"non_prime_numbers"`
 }
 
-func main() {
-	ln, err := net.Listen("tcp", ":8080")
+func errorFound(err error) {
 	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer ln.Close()
-
-	fmt.Println("Aguardando conexões TCP...")
-
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		go handleConnection(conn)
+		fmt.Println("Fatal error: ", err)
+		os.Exit(1)
 	}
 }
 
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-
-	// Receber requisição do cliente
-	buffer := make([]byte, 1024)
-	n, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Println(err)
-		return
+func isPrime(num int) bool {
+	if num == 1 {
+		return false
 	}
 
-	// Deserializar a requisição do cliente para a estrutura de requisição
-	var request NumbersRequest
-	err = json.Unmarshal(buffer[:n], &request)
-	if err != nil {
-		fmt.Println(err)
-		return
+	for i := 2; i*i <= num; i++ {
+		if num%i == 0 {
+			return false
+		}
 	}
 
-	// Separar números primos e não primos
-	primeNumbers, nonPrimeNumbers := separatePrimeNumbers(request.Numbers)
-
-	// Montar a estrutura de resposta
-	response := NumbersResponse{
-		PrimeNumbers:    primeNumbers,
-		NonPrimeNumbers: nonPrimeNumbers,
-	}
-
-	// Serializar a estrutura de resposta para JSON
-	responseJSON, err := json.Marshal(response)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// Enviar resposta para o cliente
-	_, err = conn.Write(responseJSON)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	return true
 }
 
 func separatePrimeNumbers(numbers []int) ([]int, []int) {
@@ -94,16 +49,50 @@ func separatePrimeNumbers(numbers []int) ([]int, []int) {
 	return primeNumbers, nonPrimeNumbers
 }
 
-func isPrime(num int) bool {
-	if num <= 1 {
-		return false
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	errorFound(err)
+	defer ln.Close()
+
+	fmt.Println("Aguardando conexões TCP...")
+
+	for {
+		conn, err := ln.Accept()
+		errorFound(err)
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	// Receber requisição do cliente
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	errorFound(err)
+
+	
+	// Deserializar a requisição do cliente para a estrutura de requisição
+	var request []int
+	err = json.Unmarshal(buffer[:n], &request)
+	errorFound(err)
+
+	// Separar números primos e não primos
+	primeNumbers, nonPrimeNumbers := separatePrimeNumbers(request)
+
+	// Montar a estrutura de resposta
+	response := NumbersResponse{
+		PrimeNumbers:    primeNumbers,
+		NonPrimeNumbers: nonPrimeNumbers,
 	}
 
-	for i := 2; i*i <= num; i++ {
-		if num%i == 0 {
-			return false
-		}
-	}
+	// Serializar a estrutura de resposta para JSON
+	responseJSON, err := json.Marshal(response)
+	errorFound(err)
 
-	return true
+	// Enviar resposta para o cliente
+	_, err = conn.Write(responseJSON)
+	errorFound(err)
 }
